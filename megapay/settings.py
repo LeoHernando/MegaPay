f1ebe831a85e2e8d46a7b9772ec3f5691f2b8bb7
Leo Hernando Tan <l.hernando.tan@gmail.com>
2023-04-26 23:11:23 +0800

    another deploy

--- megapay/settings.py
+++ megapay/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 from dj_database_url import parse as dburl
 from decouple import config
-config_path = '.env'
+
 
 load_dotenv()
 
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://megapay.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://megapay.herokuapp.com' ]
 
 
 # Application definition
