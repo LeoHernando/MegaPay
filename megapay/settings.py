8519b9daf902a79b5ec39c1dcbe5bd0795d4cf34
Leo Hernando Tan <l.hernando.tan@gmail.com>
2023-04-27 01:49:36 +0800

    updated settings

--- megapay/settings.py
+++ megapay/settings.py
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
