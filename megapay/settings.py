80bc80c71e9c70ff01ecc3f2b4dd9d035c13cbf9
Leo Hernando Tan <l.hernando.tan@gmail.com>
2023-04-27 03:06:50 +0800

    updated settings

--- megapay/settings.py
+++ megapay/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'megapay.herokuapp.com' ]
 
